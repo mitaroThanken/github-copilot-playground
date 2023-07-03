85083e9993b159c3ee242a257cadb67c50e6b7d9
YAMAMI Taro <mitarothanken@gmail.com>
2023-07-03 10:49:44 +0000

    ローカルで試すために位置的にコメントアウト

--- sample/chart/app/astro.config.mjs
+++ sample/chart/app/astro.config.mjs
@@ -1,7 +1,9 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
+/*
 export default defineConfig({
   site: 'https://dzeyelid.github.io',
   base: '/github-copilot-playground'
 });
+*/
